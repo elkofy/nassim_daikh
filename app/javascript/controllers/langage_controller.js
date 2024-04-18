5a13986be6a309219a3800f2e07a00a164f9271d
Nhasbeen <46243642+elkofy@users.noreply.github.com>
2024-04-18 09:02:49 +0200

    :sparkles: changed navbar

--- app/javascript/controllers/langage_controller.js
+++ app/javascript/controllers/langage_controller.js
@@ -2,14 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="langage"
 export default class extends Controller {
-  connect() {
-    console.log('Connected to langage controller')
-  }
   switch(event) {
-    const langage = event.target.innerText;
-    window.location.href = `?locale=${langage=== '🇫🇷' ? 'fr' : 'en'}`;
-    console.log(langage);
-  
+    window.location.href = `?locale=${event.target.innerText === '🇫🇷' ? 'fr' : 'en'}`;  
   }
-
 }
